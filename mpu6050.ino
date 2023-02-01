96a7bafaacae87d9450081e10530b283b3faa24e
bratik1744 <99896350+bratik1744@users.noreply.github.com>
2023-02-01 16:18:31 +0300

    Add files via upload

---
+++ mpu6050.ino
@@ -0,0 +1,25 @@
+#include "MPU6050.h"
+#define mpu_scl 1;
+#define mpu_sda 1;
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+mpu.CalibrateAccel(15);
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? "MPU6050 OK" : "MPU6050 FAIL");
+  delay(1000);
+}
+
+void loop() {
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print(ax); Serial.print('\t');
+  Serial.print(ay); Serial.print('\t');
+  Serial.print(az); Serial.print('\t');
+  Serial.print(gx); Serial.print('\t');
+  Serial.print(gy); Serial.print('\t');
+  Serial.println(gz);
+  delay(5);
+}
