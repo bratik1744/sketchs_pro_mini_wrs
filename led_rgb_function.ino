95334354a879a08a6e50f4659e3b5f3d8f047444
bratik1744 <99896350+bratik1744@users.noreply.github.com>
2023-02-01 15:38:15 +0300

    Update led_rgb_function.ino

--- led_rgb_function.ino
+++ led_rgb_function.ino
@@ -2,7 +2,7 @@
 #define led_g 1;
 #define led_b 1;
 
-void rdb_led(int red, int green, int blue){
+void rgb_led(int red, int green, int blue){
   digitalWrite(led_r, red);
   digitalWrite(led_g, green);
   digitalWrite(led_b, blue);
