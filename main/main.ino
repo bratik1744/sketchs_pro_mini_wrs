b279dcf70a6d47d9d1363fbf5b3c2739baba4ca4
bratik1744 <99896350+bratik1744@users.noreply.github.com>
2023-02-18 10:11:28 +0300

    Add files via upload

---
+++ main/main.ino
@@ -0,0 +1,10 @@
+#include "robot.h"
+Robot robot();
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ main/robot.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <Arduino.h>
+
+#include <Servo.h>
+#include <Wire.h>
+#include "MS5837.h"
+#include "MPU6050.h"
+//eeprom
+struct Mpu{
+  int16_t ax = 0;
+  int16_t ay = 0;
+  int16_t az = 0;
+  int16_t gx = 0;
+  int16_t gy = 0;
+  int16_t gz = 0;
+}
+
+struct Depth{
+  int pressure = 0;
+  int temperature = 0;
+  int depth = 0;
+  int altitude = 0;
+  
+}
+
+class Robot {
+private:
+#define led_r 5
+#define led_g 4
+#define led_b 3
+#define serv 1
+#define motor 11
+#define depth_addr 0x67
+#define mpu_addr 0x68
+
+  MPU6050 MPU;
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+  int servo_position = 0;
+  Servo myservo;
+  myservo.attach(serv);
+
+  pinMode(led_r, OUTPUT);
+  pinMode(led_g, OUTPUT);
+  pinMode(led_b, OUTPUT);
+
+  Serial.begin(57600);
+  Wire.begin();
+
+  sensor.setFluidDensity(997);
+
+public:
+  Robot();
+
+  void Robot::rgb(int red = 0, int green = 0, blue = 0) const {
+    digitalWrite(led_r, red);
+    digitalWrite(led_g, green);
+    digitalWrite(led_b, blue);
+  }
+
+  void Robot::servo(int position = 0) {
+    miservo.write(position);
+    servo_position = position;
+  }
+
+  int Robot::depth() const {
+    Depth res = {sensor.pressure(), sensor.temperature(), sensor.depth(), sensor.altitude()}
+    return res
+  }
+
+  int Robot::mpu() {
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    Mpu res = {ax, ay, az, gx, gy, gz}
+    return res;
+  }
+}
